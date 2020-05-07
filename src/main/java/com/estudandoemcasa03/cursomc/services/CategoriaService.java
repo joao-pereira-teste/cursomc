b9bdebf7efaf2c0ecb2a8655927a6a6f3043361e
Joao Pereira <joao.2009.1326@hotmail.com>
2020-05-06 21:30:48 -0300

    Criando repository e services para categoria

---
+++ src/main/java/com/estudandoemcasa03/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.estudandoemcasa03.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudandoemcasa03.cursomc.domain.Categoria;
+import com.estudandoemcasa03.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+		}
+
+}
