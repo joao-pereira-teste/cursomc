a89b1188f0d1f2b741a60c693df561e97593d120
Joao Pereira <joao.2009.1326@hotmail.com>
2020-05-07 22:28:49 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/estudandoemcasa03/cursomc/CursomcApplication.java
+++ src/main/java/com/estudandoemcasa03/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.estudandoemcasa03.cursomc.domain.Categoria;
+import com.estudandoemcasa03.cursomc.domain.Produto;
 import com.estudandoemcasa03.cursomc.repositories.CategoriaRepository;
+import com.estudandoemcasa03.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -16,7 +18,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriarepository;
-
+    @Autowired
+    private ProdutoRepository produtorepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -27,7 +31,22 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Infomatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
+		
 		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/com/estudandoemcasa03/cursomc/domain/Produto.java
+++ src/main/java/com/estudandoemcasa03/cursomc/domain/Produto.java
@@ -6,34 +6,47 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-@Entity
-public class Categoria implements Serializable {
 
-	/**
-	 * 
-	 */
+@Entity
+public class Produto implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private Integer id;
 	private String nome;
+	private Double preco;
+	
+    @ManyToMany
+    @JoinTable(name = "Produto_Categoria",
+    joinColumns = @JoinColumn(name="produto_id"),
+    inverseJoinColumns = @JoinColumn(name="categoria_id") )
+	private List<Categoria> categorias = new ArrayList<>();
 
-	@ManyToMany(mappedBy = "categorias" )
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() {
+	public Produto() {
 
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	public Integer getId() {
@@ -52,12 +65,12 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 	@Override
@@ -76,7 +89,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -84,5 +97,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+	
+	
+	
 
 }
